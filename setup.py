753fbd96f125815dc863f19b34de3042cab01920
Stefaan Lippens <soxofaan@gmail.com>
2020-06-07 11:58:55 +0200

    bump version to next alpha relase 0.4.1-alpha

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='dahuffman',
-    version='0.4.0',
+    version='0.4.1-alpha',
     description='Pure Python Huffman encoder and decoder module',
     long_description=long_description,
     url='https://github.com/soxofaan/dahuffman',
